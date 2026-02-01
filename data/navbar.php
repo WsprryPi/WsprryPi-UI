7705e67680de51c7b5b1b866a991da7475ae273d
lbussy <lee@bussy.org>
2026-02-01 12:14:21 -0600

    Brighten up log buttons

--- data/navbar.php
+++ data/navbar.php
@@ -95,7 +95,7 @@ if ($current === 'index.php') {
                         <li>
                             <a
                                 class="dropdown-item"
-                                href="https://wsprry-pi.readthedocs.io/en/latest/"
+                                href="https://wsprry-pi.readthedocs.io/en/stable/"
                                 target="_blank"
                                 rel="noopener">
                                 Documentation
